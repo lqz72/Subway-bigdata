0751a413400b2afdf8ce1aeb7a71f6a34ce8d898
lqz72 <67884710+lqz72@users.noreply.github.com>
2021-01-23 23:05:06 +0800

    安装环境

---
+++ 下载依赖(复制到Scripts中使用).bat
@@ -0,0 +1,6 @@
+pip install dash -i https://douban.com/simple
+pip install numpy-i https://mirrors.aliyun.com/pypi/simple/
+pip install pandas -i https://mirrors.aliyun.com/pypi/simple/
+pip install matplotlib -i https://douban.com/simple
+pause
+
