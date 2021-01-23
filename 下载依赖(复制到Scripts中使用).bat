9d245b62c92268fb37feb2d3dd4999f6ef0aca6c
lqz72 <67884710+lqz72@users.noreply.github.com>
2021-01-23 23:07:12 +0800

    安装环境更新

--- 下载依赖(复制到Scripts中使用).bat
+++ 下载依赖(复制到Scripts中使用).bat
@@ -1,6 +1,5 @@
-pip install dash -i https://douban.com/simple
-pip install numpy-i https://mirrors.aliyun.com/pypi/simple/
-pip install pandas -i https://mirrors.aliyun.com/pypi/simple/
-pip install matplotlib -i https://douban.com/simple
+pip install dash -i http://pypi.douban.com/simple --trusted-host pypi.douban.com
+pip install numpy -i http://pypi.douban.com/simple --trusted-host pypi.douban.com
+pip install pandas -i http://pypi.douban.com/simple --trusted-host pypi.douban.com
+pip install matplotlib -i http://pypi.douban.com/simple --trusted-host pypi.douban.com
 pause
-
