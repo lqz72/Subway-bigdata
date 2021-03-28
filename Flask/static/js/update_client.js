434b92d5fff7d3c13451441c615f0eebaa8478bb
iaaaaaaaaaaaa <66128020+iaaaaaaaaaaaa@users.noreply.github.com>
2021-03-28 20:17:52 +0800

    半成品+测试用

--- Flask/static/js/update_client.js
+++ Flask/static/js/update_client.js
@@ -1,36 +1,22 @@
-$(
-    function chart() {
-        var age_bar = echarts.init(document.getElementById('age_bar'));
-        var age_pie = echarts.init(document.getElementById('age_pie'));
+$(function(){
+    // 点击详细信息
+    var btn1 = document.querySelector('#btn_show');
+        btn1.addEventListener('click',
+        function()
+        {
+            location.href = '/userinf';
+        })
 
-        $.ajax({
-            type: 'POST',
-            url: 'history/age/pie',
-        
-            dataType: 'json',
-            success: function (result) {
-                age_pie.setOption(result);
-            }
-        });
-
-        $.ajax({
-            type: 'POST',
-            url: 'history/age/bar',
-        
-            dataType: 'json',
-            success: function (result) {
-                age_bar.setOption(result);
-            }
-        });
-
-        var user_id = "d4ec5a712f2b24ce226970a8d315dfce";
+    //点击搜索后
+    var btn_search = document.getElementById('btn_search');
+    var user_id = document.getElementById('user_search');
+    btn_search.onclick = function(){
         $.ajax({
             url: '/user_info',
             type: 'POST',
-            data: user_id,
+            data: user_id.value,
             dataType: 'json',
             success: function(result){
-                console.log(result)
                 var id = document.getElementById('id');
                 var age = document.getElementById('age');
                 var trips_num = document.getElementById('trips_num');
@@ -44,15 +30,96 @@ $(
         $.ajax({
             type: 'POST',
             url: '/history/user_flow/line',
-            data: user_id,
+            data: user_id.value,
             dataType: 'json',
             success: function (result) {        
                 user_flow_line.setOption(result);
             }
         });
 
-    },
+        //出行记录展示
+        $.ajax({
+            type:'post',
+            url:'/user_record',
+            data: user_id.value,
+            success: function(data)
+            {
+                console.log(data);
+                test = {title:'用户记录'};
+                test['list'] = data;
+                test['length'] = data.length;
+                console.log(test);
+                var html = template('historyshow', test);
+                document.getElementById('outshow1').innerHTML = html;
+            }
+        });
+    }
 
+    //顶上俩表
+    var age_bar = echarts.init(document.getElementById('age_bar'));
+    var age_pie = echarts.init(document.getElementById('age_pie'));
+
+    $.ajax({
+        type: 'POST',
+        url: 'history/age/pie',
     
+        dataType: 'json',
+        success: function (result) {
+            age_pie.setOption(result);
+        }
+    });
 
+    $.ajax({
+        type: 'POST',
+        url: 'history/age/bar',
+    
+        dataType: 'json',
+        success: function (result) {
+            age_bar.setOption(result);
+        }
+    });
+
+    //默认显示
+    var init_id = "d4ec5a712f2b24ce226970a8d315dfce";
+    $.ajax({
+        url: '/user_info',
+        type: 'POST',
+        data: init_id,
+        dataType: 'json',
+        success: function(result){
+            // console.log(result);
+            var id = document.getElementById('id');
+            var age = document.getElementById('age');
+            var trips_num = document.getElementById('trips_num');
+            id.innerHTML = result.id;
+            age.innerHTML = result.age;
+            trips_num.innerHTML = result.trips_num;
+        }
+    });
+
+    var user_flow_line = echarts.init(document.getElementById('monthout'));
+    
+    $.ajax({
+        type: 'POST',
+        url: '/history/user_flow/line',
+        data: init_id,
+        dataType: 'json',
+        success: function (result) {        
+            user_flow_line.setOption(result);
+        }
+    });
+    $.ajax({
+        type:'post',
+        url:'/user_record',
+        data: init_id,
+        success: function(data)
+        {
+            console.log(data);
+            test = {title:'用户记录'};
+            test['list'] = data;
+            var html = template('historyshow', test);
+            document.getElementById('outshow1').innerHTML = html;
+        }
+    });
+}
 )
---
+++ Flask/static/js/userinf.js
@@ -0,0 +1,41 @@
+layui.use('laypage', function(){
+    var laypage = layui.laypage;
+    
+    //执行一个laypage实例
+    laypage.render({
+    elem: 'pageselector' //注意，这里的 test1 是 ID，不用加 # 号
+    ,count: 24782 //数据总数，从服务端得到
+    ,limit:15
+    ,groups:6
+    ,layout:['prev', 'page', 'next','refresh','skip']
+    ,jump: function(obj, first){
+        //obj包含了当前分页的所有参数，比如：
+        console.log(obj.curr); //得到当前页，以便向服务端请求对应页的数据。
+        console.log(obj.limit); //得到每页显示的条数
+        
+        $.ajax({
+            type:'get',
+            url:'/users_info/' + obj.curr,
+            success: function(data)
+            {
+                var t = {};
+                t.tt = '这是怎么回事';
+                t.val = data;
+                console.log(t);
+                var html = template('test', t);
+                document.getElementById('userlist').innerHTML = html;
+                
+            }
+        })
+
+        //首次不执行
+        if(!first){
+        //do something
+        }
+        }
+        });
+});
+
+
+
+
