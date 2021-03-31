747b96dca7c11a498fadff79626516cde76c0ff8
iaaaaaaaaaaaa <66128020+iaaaaaaaaaaaa@users.noreply.github.com>
2021-03-31 17:23:55 +0800

    前端实现对管理员数据的增删改
    等待写后端接口

--- Flask/static/js/selfcenter.js
+++ Flask/static/js/selfcenter.js
@@ -10,12 +10,86 @@ $(function(){
             document.getElementById('adminlist').innerHTML = html;
 
             var ad_list = document.querySelector('#adminlist').children[0];
-            console.log(data.length);
             for(var i=0;i<data.length;i++)
             {
-                // console.log(ad_list.children[0].children[1]);
-                ad_list.children[i+1].children[3].children[0].children[0].addEventListener('click',function(){
-                    console.log(this.parentNode.parentNode.parentNode.children[0].innerText);
+                var edit = ad_list.children[i+1].children[3].children[0].children[0];
+                edit.setAttribute("index",i);
+                edit.addEventListener('click',function(){
+                    // console.log(this.getAttribute("index"));
+                    // 0表示第一个元素
+                    //向后台发送编辑index请求
+                    var top = this.parentNode.parentNode.parentNode;
+                    var v_username = top.children[0].innerHTML;
+                    var v_pwd = top.children[1].innerHTML;
+                    var v_tips = top.children[2].innerHTML;
+                    var past_inf = {};
+                    past_inf.username = v_username;
+                    past_inf.pwd = v_pwd;
+                    past_inf.tips = v_tips;
+                    $('#testmodal').on('show.bs.modal', function (event) {
+                        var modal = $(this);
+                        modal.find('#m-username').val(past_inf.username);
+                        modal.find('#m-pwd').val(past_inf.pwd);
+                        modal.find('#m-tips').val(past_inf.tips);
+                        
+                    })
+                    $('#testmodal').modal();
+                    var data_to_back = {};
+                    var username = document.querySelector('#m-username');
+                    var pwd = document.querySelector('#m-pwd');
+                    var tips = document.querySelector('#m-tips');
+                    var inf = {};
+                    inf.username = username.value;
+                    inf.pwd = pwd.value;
+                    inf.tips = tips.value;
+                    data_to_back.inf = inf;
+                    data_to_back.index = this.getAttribute("index");
+                    var confirm = document.querySelector('#confirm_edit');
+                    confirm.addEventListener('click',function(){
+                        // console.log(data_to_back);
+                        $.ajax({
+                            type:'GET',
+                            url:'/updata_database',
+                            data:inf,
+                            success: function(data)
+                            {
+                                if(data)
+                                {
+                                    alert("修改成功！");
+                                    location.href = '/selfcenter';
+                                }
+                            }
+                        })
+                    })
+                })
+                var del = edit.nextElementSibling;
+                del.setAttribute("index",i);
+                del.addEventListener("click",function(){
+                    console.log(this.getAttribute("index"));
+                    var index = this.getAttribute("index");
+                    //向后台发送删除index请求
+                    var r=confirm("你确定要删除吗？")
+                    if (r==true)
+                        {
+                            $.ajax({
+                                type:'GET',
+                                url:'/del_inf',
+                                data:index,
+                                success: function(data)
+                                {
+                                    if(data)
+                                    {
+                                        alert("删除成功！");
+                                        location.href = '/selfcenter';
+                                    }
+                                }
+                            })
+                        }
+                    else
+                        {
+                            
+                        }
+                    
                 })
             }
         }
@@ -35,10 +109,13 @@ $(function(){
             type:'GET',
             url:'',
             data:inf,
-            success: function()
+            success: function(data)
             {
-                alert("创建用户成功！");
-                location.href = '/selfcenter';
+                if(data)
+                {
+                    alert("创建用户成功！");
+                    location.href = '/selfcenter';
+                }
             }
         })
 
