3c8592e79deb353ba1052bce83d3bad2bd09512a
iaaaaaaaaaaaa <66128020+iaaaaaaaaaaaa@users.noreply.github.com>
2021-04-14 19:55:22 +0800

    词云等

--- Flask/static/js/sta.js
+++ Flask/static/js/sta.js
@@ -1,6 +1,5 @@
 layui.use('laydate', function(){
     var laydate = layui.laydate;
-    
     //执行一个laydate实例
     laydate.render({
         elem: '#timectrlsta'
@@ -69,3 +68,100 @@ var option_marksta = {
         }]
     }]
 };
+
+// ---------词云
+var chart = echarts.init(document.getElementById('wordclouds'));
+
+      var option = {
+          tooltip: {},
+          series: [ {
+              type: 'wordCloud',
+              gridSize: 2,
+              sizeRange: [6, 30],
+              // sizeRange: [12, 50],
+              rotationRange: [-90, 90],
+              shape: 'pentagon',
+              width: 226,
+              height: 190,
+              drawOutOfBound: true,
+              textStyle: {
+                  color: function () {
+                      return 'rgb(' + [
+                          Math.round(Math.random() * 160),
+                          Math.round(Math.random() * 160),
+                          Math.round(Math.random() * 160)
+                      ].join(',') + ')';
+                  }
+              },
+              emphasis: {
+                  textStyle: {
+                      shadowBlur: 10,
+                      shadowColor: '#333'
+                  }
+              },
+              data: [
+                  {
+                      name: '通勤乘客',
+                      value: 10000,
+                      textStyle: {
+                          color: 'black'
+                      },
+                      emphasis: {
+                          textStyle: {
+                              color: 'red'
+                          }
+                      }
+                  },
+                  {
+                    name:"剁手党",
+                    value: 9100
+                  },
+                  {
+                    name:"购物达人",
+                    value: 3200
+                  },
+                  {
+                    name:"驴友",
+                    value: 8100
+                  },
+                  {
+                    name:"学生党",
+                    value: 6000
+                  },
+                  {
+                    name:"远距通勤者",
+                    value: 5100
+                  },
+                  {
+                    name:"夜行人",
+                    value: 3100
+                  },
+                  {
+                    name:"学生",
+                    value: 3100
+                  },
+                  {
+                    name:"女性用户",
+                    value: 2500
+                  },
+                  {
+                    name:"夜猫子",
+                    value: 2000
+                  },
+                  {
+                    name:"shopping族",
+                    value: 3100
+                  },
+                  {
+                    name:"高龄乘客",
+                    value: 1800
+                  },
+                  {
+                    name:"行动不便者",
+                    value: 1200
+                  }
+              ]
+          } ]
+      };
+
+      chart.setOption(option);
